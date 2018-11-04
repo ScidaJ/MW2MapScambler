15cb94765b46f1a696ed613b8201fc7c88dd6a8a
jrs8979 <jrs8979@g.rit.edu>
2018-11-04 12:17:14 -0500

    Changed ArrayList storage to HashMap.
    Moved the ArrayList used to store the map objects over to a HashMap. Had to revert back to ForLoop to generate the random map list. Added printing out of the plain text map names in order to the console. Hoping to rework this in the future.

--- src/MW2MapScrambler.java
+++ src/MW2MapScrambler.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.util.Map;
 import java.util.Random;
 
 public class MW2MapScrambler {
@@ -8,10 +8,22 @@ public class MW2MapScrambler {
     public static void main(String[] args) {
         String mapFile = "d://Games//New folder (2)//maps.txt";
         String execFile = "d://Games//New folder (2)//startserver.bat";
-        ArrayList<Maps> mapList = MapListImport.importMap(mapFile);
-        Random rand = new Random(mapList.size());
-        String exec = mapList.stream().map(e -> mapList.get(rand.nextInt(mapList.size())).getInGame() + " ").reduce("@echo off\n" +
-                "start iw4x.exe -dedicated set net_port 28961 +exec server.cfg +party_enable 0 +sv_maxclients 16 +set lan_only 1 +set fs_game \"mods/bots\" +set sv_mapRotation \"", String::concat);
+        Map<String, String> mapList = MapListImport.importMap(mapFile);
+        int size = mapList.size();
+        Random rand = new Random(size);
+        String exec = "@echo off\n" +
+                "start iw4x.exe -dedicated set net_port 28961 +exec server.cfg +party_enable 0 +sv_maxclients 16"
+                + "set lan_only 1 +set fs_game \"mods/bots\" +set sv_mapRotation \"";
+        String[] k = mapList.keySet().toArray(new String[size]);
+        for(int i = 0;i < size;i++){
+            String map = k[rand.nextInt(size)];
+            if(i < size - 1){
+                exec += mapList.get(map) + " ";
+            } else {
+                exec += mapList.get(map);
+            }
+            System.out.println(map);
+        }
         exec += "\" +map_rotate +set g_gametype dom +set bots_manage_fill 12 +set scr_xpscale 4";
         try {
             PrintWriter out = new PrintWriter(execFile);
--- src/MapListImport.java
+++ src/MapListImport.java
@@ -2,18 +2,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class MapListImport {
 
-    public static ArrayList<Maps> importMap(String fileName){
-        ArrayList<Maps> mapList = new ArrayList<>();
+    public static Map<String, String> importMap(String fileName){
+        Map<String, String> mapList = new HashMap<>();
         try(Stream<String> stream = Files.lines(Paths.get(fileName))){
             String[] maps = stream.toArray(String[]::new);
             for(String s: maps){
                 String[] mapHold = s.split(",");
                 Maps map = new Maps(mapHold[0], mapHold[1]);
-                mapList.add(map);
+                mapList.put(map.getPlainText(), map.getInGame());
             }
             return mapList;
         } catch(IOException e){
