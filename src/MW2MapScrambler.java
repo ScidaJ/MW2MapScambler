490f0745551824656f3931729afe9ea555ca1cbe
jrs8979 <jrs8979@g.rit.edu>
2018-11-04 18:51:57 -0500

    Changed random sorting
    No longer using Collections.shuffle. Ask me about how it works if you can't understand my map of maps.

--- src/MW2MapScrambler.java
+++ src/MW2MapScrambler.java
@@ -1,28 +1,23 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.*;
+
 
 public class MW2MapScrambler {
 
     public static void main(String[] args) {
         String mapFile = "d://Games//New folder (2)//maps.txt";
         String execFile = "d://Games//New folder (2)//startserver.bat";
-        Map<String, String> mapList = MapListImport.importMap(mapFile);
+        MapListImport.importMap(mapFile);
         String exec = "@echo off\n" +
                 "start iw4x.exe -dedicated set net_port 28961 +exec server.cfg +party_enable 0 +sv_maxclients 16"
                 + "set lan_only 1 +set fs_game \"mods/bots\" +set sv_mapRotation \"";
-        List<String> maps = new ArrayList<>(mapList.keySet());
-        Collections.shuffle(maps);
-        Iterator<String> iterator = maps.iterator();
-        while(iterator.hasNext()){
-            String m = iterator.next();
-            if(iterator.hasNext()) {
-                exec += mapList.get(m) + " ";
-                System.out.println(m);
+        for(int i = 0;i < MapListImport.mapList.size();i++){
+            if(i < MapListImport.mapList.size() - 1){
+                exec += MapListImport.mapContainer.get(MapListImport.mapList.get(i)) + " ";
             } else {
-                exec += mapList.get(m);
-                System.out.println(m);
+                exec += MapListImport.mapContainer.get(MapListImport.mapList.get(i));
             }
+            System.out.println(MapListImport.mapList.get(Integer.valueOf(i)));
         }
         exec += "\" +map_rotate +set g_gametype dom +set bots_manage_fill 12 +set scr_xpscale 4";
         try {
--- src/MapListImport.java
+++ src/MapListImport.java
@@ -3,19 +3,34 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.Stream;
 
 public class MapListImport {
+    static Map<Integer, String> mapList;
+    static Map<String, String> mapContainer;
 
-    public static Map<String, String> importMap(String fileName){
-        Map<String, String> mapList = new HashMap<>();
+    public static Map<Integer, String> importMap(String fileName){
+        mapList = new HashMap<>();
+        mapContainer = new HashMap<>();
         try(Stream<String> stream = Files.lines(Paths.get(fileName))){
             String[] maps = stream.toArray(String[]::new);
+            int size = (maps.length);
+            Random rand = new Random();
             for(String s: maps){
+                Integer integer = new Integer(rand.nextInt(size));
+                while(mapList.containsKey(integer)){
+                    if(integer != 0){
+                        integer = integer - 1;
+                    } else {
+                        integer = size;
+                    }
+                }
                 String[] mapHold = s.split(",");
                 String inGame = mapHold[0];
                 String plainText = mapHold[1];
-                mapList.put(plainText, inGame);
+                mapContainer.put(plainText, inGame);
+                mapList.put(integer, plainText);
             }
             return mapList;
         } catch(IOException e){
