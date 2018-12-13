7a92af435b0e48fdbbc45156b0786ba3a8ebd70a
jrs8979 <jrs8979@rit.edu>
2018-12-12 19:43:55 -0500

    Added logs instead of console messages. Using StringBuilders instead of appending Strings.

--- src/MW2MapScrambler.java
+++ src/MW2MapScrambler.java
@@ -1,31 +1,45 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class MW2MapScrambler {
 
+    static Logger logger = Logger.getLogger(MW2MapScrambler.class.getName());
+
     public static void main(String[] args) {
+        try {
+            logger.addHandler(new FileHandler("Scrambler.log"));
+        } catch(IOException e){
+            logger.log(Level.SEVERE, e.toString(), e);
+        }
         String mapFile = "d://Games//New folder (2)//maps.txt";
         String execFile = "d://Games//New folder (2)//startserver.bat";
         MapListImport.importMap(mapFile);
-        String exec = "@echo off\n" +
-                "start iw4x.exe -dedicated set net_port 28961 +exec server.cfg +party_enable 0 +sv_maxclients 16"
-                + " +set lan_only 1 +set fs_game \"mods/bots\" +set sv_mapRotation \"";
+        StringBuilder bld = new StringBuilder();
+        bld.append("@echo off\n");
+        bld.append("start iw4x.exe -dedicated set net_port 28961 +exec server.cfg +party_enable 0 +sv_maxclients 16");
+        bld.append(" +set lan_only 1 +set fs_game \"mods/bots\" +set sv_mapRotation \"");
         for(int i = 0;i < MapListImport.mapList.size();i++){
             if(i < MapListImport.mapList.size() - 1){
-                exec += "map " + MapListImport.mapContainer.get(MapListImport.mapList.get(i)) + " ";
+                bld.append("map ");
+                bld.append(MapListImport.mapContainer.get(MapListImport.mapList.get(i)));
+                bld.append(" ");
             } else {
-                exec += "map " + MapListImport.mapContainer.get(MapListImport.mapList.get(i));
+                bld.append("map ");
+                bld.append(MapListImport.mapContainer.get(MapListImport.mapList.get(i)));
             }
-            System.out.println(MapListImport.mapList.get(Integer.valueOf(i)));
+            logger.log(Level.INFO, MapListImport.mapList.get(i));
         }
-        exec += "\" +map_rotate +set g_gametype dom +set bots_manage_fill 12 +set scr_xpscale 4";
+        bld.append("\" +map_rotate +set g_gametype dom +set bots_manage_fill 12 +set scr_xpscale 4");
         try {
             PrintWriter out = new PrintWriter(execFile);
-            out.println(exec);
+            out.println(bld.toString());
             out.close();
         } catch(IOException e){
-            System.err.print(e);
+            logger.log(Level.SEVERE,e.toString(), e);
         }
     }
 }
--- src/MapListImport.java
+++ src/MapListImport.java
@@ -1,16 +1,29 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 public class MapListImport {
     static Map<Integer, String> mapList;
     static Map<String, String> mapContainer;
+    static Logger logger = Logger.getLogger(MW2MapScrambler.class.getName());
 
-    public static Map<Integer, String> importMap(String fileName){
+    private MapListImport(){}
+
+    public static void importMap(String fileName){
+        try{
+            logger.addHandler(new FileHandler("Import.log"));
+        } catch(IOException e){
+            logger.log(Level.SEVERE, e.toString(), e);
+        }
         mapList = new HashMap<>();
         mapContainer = new HashMap<>();
         try(Stream<String> stream = Files.lines(Paths.get(fileName))){
@@ -18,7 +31,7 @@ public class MapListImport {
             int size = (maps.length - 1);
             Random rand = new Random();
             for(String s: maps){
-                Integer integer = new Integer(rand.nextInt(size));
+                int integer = rand.nextInt(size);
                 while(mapList.containsKey(integer)){
                     if(integer != 0){
                         integer = integer - 1;
@@ -27,15 +40,13 @@ public class MapListImport {
                     }
                 }
                 String[] mapHold = s.split(",");
-                String inGame = mapHold[0].toString();
-                String plainText = mapHold[1].toString();
+                String inGame = mapHold[0];
+                String plainText = mapHold[1];
                 mapContainer.put(plainText, inGame);
                 mapList.put(integer, plainText);
             }
-            return mapList;
         } catch(IOException e){
-            System.err.print(e);
+            logger.log(Level.SEVERE, e.toString(), e);
         }
-        return null;
     }
 }
